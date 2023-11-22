93373a5c7ccf2a55bfe4563e1dc869b07b6c0edc
Hardik Patel <hardik.patel@leaseplan.com>
2023-11-22 18:01:17 +0100

    feat: Initial UI layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {StyledEngineProvider} from '@mui/material/styles';
+import devoteamTheme from './themes/devoteamTheme';
+import {ThemeProvider} from "@mui/material";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={devoteamTheme}> {/* Apply the theme */}
+                <App/>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/themes/devoteamTheme.ts
@@ -0,0 +1,20 @@
+// theme.js or theme.ts
+import { createTheme } from '@mui/material/styles';
+
+const devoteamTheme = createTheme({
+    // Define your theme settings here
+    palette: {
+        primary: {
+            main: '#f8485e',
+        },
+        secondary: {
+            main: '#fca2ae',
+        },
+        background: {
+            default: '#ffffff',
+        },
+    },
+    // You can also customize typography, breakpoints, etc.
+});
+
+export default devoteamTheme;
