07641471b8c917a6ffca7f400684750ef2940839
Kyle Watson <kyle.watson@devoteam.com>
2023-11-27 15:47:45 +0100

    Install prettier, Run prettier on all files, Align more info button with plugins selection, Label plugins with correct text.

--- src/components/PluginSelector/PluginSelector.tsx
+++ src/components/PluginSelector/PluginSelector.tsx
@@ -1,94 +1,99 @@
-import * as React from 'react';
-import { Theme, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import Chip from '@mui/material/Chip';
+import HelpOutlineSharpIcon from "@mui/icons-material/HelpOutlineSharp";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import HelpOutlineSharpIcon from '@mui/icons-material/HelpOutlineSharp';
-import Divider from '@mui/material/Divider';
-
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { Theme, useTheme } from "@mui/material/styles";
+import * as React from "react";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
-        },
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
     },
+  },
 };
 
 const names = [
-    'Joan',
-    'DevoteamNL',
-    'DevoteamPortugal',
-    'ElasticSearch',
-    'SmartRecruiters'
+  "Joan",
+  "DevoteamNL",
+  "DevoteamPortugal",
+  "ElasticSearch",
+  "SmartRecruiters",
 ];
 
 function getStyles(name: string, personName: readonly string[], theme: Theme) {
-    return {
-        fontWeight:
-            personName.indexOf(name) === -1
-                ? theme.typography.fontWeightRegular
-                : theme.typography.fontWeightMedium,
-    };
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
 }
 
 export default function PluginSelector() {
-    const theme = useTheme();
-    const [personName, setPersonName] = React.useState<string[]>([]);
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState<string[]>([]);
 
-    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
-        const {
-            target: { value },
-        } = event;
-        setPersonName(
-            // On autofill we get a stringified value.
-            typeof value === 'string' ? value.split(',') : value,
-        );
-    };
-
-    return (
-        <Box>
-                <FormControl sx={{ m: 1, width: 300 }}>
-                    <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
-                    <Select
-                        labelId="demo-multiple-chip-label"
-                        id="demo-multiple-chip"
-                        multiple
-                        value={personName}
-                        onChange={handleChange}
-                        input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                        renderValue={(selected) => (
-                            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                                {selected.map((value) => (
-                                    <Chip key={value} label={value} />
-                                ))}
-                            </Box>
-                        )}
-                        MenuProps={MenuProps}
-                    >
-                        {names.map((name) => (
-                            <MenuItem
-                                key={name}
-                                value={name}
-                                style={getStyles(name, personName, theme)}
-                            >
-                                {name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <Button component="label" variant="contained" endIcon={<HelpOutlineSharpIcon />}>
-                    More Info
-                </Button>
-            <Divider/>
-        </Box>
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value,
     );
+  };
+
+  return (
+    <Box>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel id="demo-multiple-chip-label">Plugin</InputLabel>
+          <Select
+            labelId="demo-multiple-chip-label"
+            id="demo-multiple-chip"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<OutlinedInput id="select-multiple-chip" label="Plugin" />}
+            renderValue={(selected) => (
+              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip key={value} label={value} />
+                ))}
+              </Box>
+            )}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, personName, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button
+          component="label"
+          variant="contained"
+          endIcon={<HelpOutlineSharpIcon />}
+        >
+          More Info
+        </Button>
+      </Box>
+      <Divider />
+    </Box>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {StyledEngineProvider} from '@mui/material/styles';
-import devoteamTheme from './themes/devoteamTheme';
-import {ThemeProvider} from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import { StyledEngineProvider } from "@mui/material/styles";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import devoteamTheme from "./themes/devoteamTheme";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
-    <React.StrictMode>
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={devoteamTheme}> {/* Apply the theme */}
-                <App/>
-            </ThemeProvider>
-        </StyledEngineProvider>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={devoteamTheme}>
+        {" "}
+        {/* Apply the theme */}
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
