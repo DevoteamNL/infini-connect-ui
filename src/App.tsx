99ad8c86e774e172f762607b1e871743c073e289
Kyle Watson <kyle.watson@devoteam.com>
2023-11-30 13:10:46 +0100

    Add a proper sign in page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { GoogleLogin } from "@react-oauth/google";
 import { useContext } from "react";
 import ChatHistoryDrawer from "./components/ChatList/ChatHistoryDrawer";
+import SignIn from "./components/SignIn/SignIn";
 import { AuthContext } from "./context/AuthContext";
 function App() {
-  const { loggedIn, login } = useContext(AuthContext);
+  const { loggedIn } = useContext(AuthContext);
 
-  return loggedIn ? (
-    <ChatHistoryDrawer />
-  ) : (
-    <GoogleLogin onSuccess={login} onError={console.error} />
-  );
+  return loggedIn ? <ChatHistoryDrawer /> : <SignIn />;
 }
 
 export default App;
---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,53 @@
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Box, Stack } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import { GoogleLogin } from "@react-oauth/google";
+import * as React from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+const SignIn = () => {
+  const { login } = React.useContext(AuthContext);
+
+  return (
+    <Stack
+      spacing={4}
+      alignItems="center"
+      sx={{
+        paddingTop: 8,
+        paddingBottom: 8,
+        height: "100vh",
+      }}
+      component="main"
+      maxWidth="xs"
+    >
+      <CssBaseline />
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5" marginBottom={8}>
+        Sign in to InfiniConnect
+      </Typography>
+
+      <Box flex={1}>
+        <GoogleLogin onSuccess={login} onError={console.error} useOneTap />
+      </Box>
+
+      <img src="/static/images/devoteam_rgb.png" alt="logo" width="200px" />
+
+      <Typography variant="body2" color="text.secondary" align="center">
+        For internal Devoteam use only!{" "}
+        <Link
+          color="inherit"
+          href="https://github.com/DevoteamNL/infini-connect-ui"
+        >
+          InfiniConnect
+        </Link>
+      </Typography>
+    </Stack>
+  );
+};
+
+export default SignIn;
