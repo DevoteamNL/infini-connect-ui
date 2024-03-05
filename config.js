a04045b13f45116e7fd353947c23ef353983e61e
Hardik Patel <hardik.patel@devoteam.com>
2024-03-05 16:05:49 +0100

    Fix for env variables

---
+++ config.js
@@ -0,0 +1,5 @@
+window.config = {
+  VITE_API_BASE_URL: "http://localhost:8080",
+  VITE_GOOGLE_CLIENT_ID:
+    "779787791084-sf0tjf22s5cl3odf9sjg6rch0if9c0qq.apps.googleusercontent.com",
+};
---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+interface Window {
+  config: {
+    VITE_API_BASE_URL?: string;
+    VITE_GOOGLE_CLIENT_ID?: string;
+  };
+}
